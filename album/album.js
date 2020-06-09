3e1d7ae27538e9ba30c6d87cd285694b98a0d768
Elke <elke@surprisesurprise.at>
2020-06-09 15:59:36 +0200

    updated album

--- album/album.js
+++ album/album.js
@@ -1,8 +1,10 @@
 let getWrapper = document.getElementById("wrapper");
 
+
 for (i = 0; i < people.length; i++) {
     getWrapper.innerHTML += `<div class="container"><img src="${people[i].img}"><div><p>${people[i].name}, ${people[i].age}</p></div></div>`;
     // console.log(cars[i].brand, cars[i].model)
+
 }
 
 
